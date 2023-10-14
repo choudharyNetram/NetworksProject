45dc1bcc4e276fe4706dec0c5abe1ddc6568314e
choudharyNetram <choudharynetram7@gmail.com>
2023-10-14 18:46:24 +0530

    backend authentication is completed

--- backend/src/Controllers/AdminAuthController.js
+++ backend/src/Controllers/AdminAuthController.js
@@ -30,7 +30,46 @@ module.exports.adminSignup = async (req, res, next) => {
 };
 
 
+module.exports.adminLogin = async (req, res, next) => {
+    try {
+      const { email, password } = req.body;
+      if (!email || !password) {
+        return res.status(400).json({ message: 'Email and password are required' });
+      }
+  
+      const admin = await Admin.findOne({ email });
+      if (!admin) {
+        return res.status(401).json({ message: 'Invalid email or password, Invalid Admin' });
+      }
+  
+      const isPasswordValid = await bcrypt.compare(password, admin.password);
+      if (!isPasswordValid) {
+        return res.status(401).json({ message: 'Invalid password' });
+      }
+  
+      // Check if the admin is approved
+      if (!admin.isApproved) {
+        return res.status(401).json({ message: 'Your Admin request is not accepted yet' });
+      }
+  
+      // Generate a token for the authenticated admin
+      const token = createSecretToken(admin._id);
+  
+      res.cookie('token', token, {
+        withCredentials: true,
+        httpOnly: false,
+      });
+  
+      res.status(200).json({ message: 'Admin logged in successfully', success: true, admin });
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+};
+  
 
+/*
 module.exports.adminLogin = async (req, res, next) => {
   try {
     const { email, password } = req.body;
@@ -63,3 +102,4 @@ module.exports.adminLogin = async (req, res, next) => {
     res.status(500).json({ message: 'Internal server error' });
   }
 };
+*/
